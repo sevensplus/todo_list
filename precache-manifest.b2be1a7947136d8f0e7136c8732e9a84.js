c9781a984b65de9d95b1af9b83a9124c3c6a4f3a
unknown <kebcdy@gmail.com>
2020-01-04 14:58:18 +0800

    Updates

---
+++ precache-manifest.b2be1a7947136d8f0e7136c8732e9a84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41031afde02498925499df929ec22cd2",
+    "url": "/todo_list/index.html"
+  },
+  {
+    "revision": "4a803b674dcbafde7fad",
+    "url": "/todo_list/static/css/main.f28dde5a.chunk.css"
+  },
+  {
+    "revision": "3bf93753593c34d36371",
+    "url": "/todo_list/static/js/2.e9c46e05.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todo_list/static/js/2.e9c46e05.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4a803b674dcbafde7fad",
+    "url": "/todo_list/static/js/main.35377afd.chunk.js"
+  },
+  {
+    "revision": "cab1900f081f6b945a7b",
+    "url": "/todo_list/static/js/runtime-main.57643561.js"
+  }
+]);
